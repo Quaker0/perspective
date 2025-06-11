2a7f582b1a5a2fc57a33d1b235276fc90224e2ed
Niclas <niclas@funnel.io>
2025-06-11 16:17:23 +0200

    fix: user typing

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,17 @@
 import UserDatabaseClient from '../database/UserDatabaseClient';
-import User from '../models/User';
-import { CreateUserInput } from '../validators/userValidators';
+import User, { UserDisplayData } from '../models/User';
+import { CreateUserBody } from '../schemas/userSchemas';
 
 const databaseClient = new UserDatabaseClient();
 
-export async function createUser(userInput: CreateUserInput): Promise<void> {
-    const user = User.fromObject(userInput);
+export async function createUser(userInput: CreateUserBody): Promise<void> {
+    const user = User.create(userInput);
     await databaseClient.createUser(user);
 }
 
-export async function getUsers(created: 'asc' | 'desc'): Promise<any[]> {
-    // TODO: fix typing
+export async function getUsers(created: 'asc' | 'desc'): Promise<UserDisplayData[]> {
     const userDataArray = await databaseClient.getUserDocuments(created);
-    return userDataArray.map((userData) => User.fromObject(userData).toOutput());
+    return userDataArray.map((userData) => User.fromObject(userData).toDisplay());
 }
 
 export async function connectUserDatabase(): Promise<void> {
